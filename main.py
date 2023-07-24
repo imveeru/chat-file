b61e3e74b0ac99e73c9e215c523aeea5bd7b53dd
imvee <vind.veeru@gmail.com>
2023-07-24 13:59:38 +0530

    File upload

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.title("📈ChatFile")
 
+st.file_uploader("Upload your file!")
+st.caption("Only CSV and PDF files are supported.")
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -9,7 +12,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Ask your question..."):
     st.chat_message("user").markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
 
