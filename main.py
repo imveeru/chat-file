365246c299dab637fd982e8c610f8c027f89a167
imvee <vind.veeru@gmail.com>
2023-07-24 14:02:49 +0530

    File upload

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ import streamlit as st
 
 st.title("📈ChatFile")
 
-st.file_uploader("Upload your file!")
+uploaded_file=st.file_uploader("Upload your file!")
 st.caption("Only CSV and PDF files are supported.")
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+if uploaded_file is not None:
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-if prompt := st.chat_input("Ask your question..."):
-    st.chat_message("user").markdown(prompt)
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    if prompt := st.chat_input("Ask your question..."):
+        st.chat_message("user").markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f"Echo: {prompt}"
-    
-    with st.chat_message("assistant"):
-        st.markdown(response)
+        response = f"Echo: {prompt}"
         
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        with st.chat_message("assistant"):
+            st.markdown(response)
+            
+        st.session_state.messages.append({"role": "assistant", "content": response})
